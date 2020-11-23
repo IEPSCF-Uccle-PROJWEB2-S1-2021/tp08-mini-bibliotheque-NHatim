f9a5c87f5a596c1fc7e5ca1183f3377dd6c43e47
NHatim <hatim.naimi@gmail.com>
2020-11-23 15:51:50 +0100

    End commit

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('view engine', 'pug')
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -35,5 +37,6 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,59 +1,68 @@
 const express = require('express');
+
 const router = new express.Router();
 const books = [{
   author: "Guillaume Debré",
   title: "L'affaire Lafayette",
-  category: "roman historique"
+  category: "Roman Historique"
 },{
   author: "Gérald Messadié",
   title: "La conspiration Jeanne d'Arc",
-  category: "roman historique"
+  category: "Roman Historique"
 },{
   author: "J.R.R. Tolkien",
   title: "Le Seigneur des anneaux",
-  category: "fantasy"
+  category: "Fantasy"
 },{
   author: "Michael Ende",
   title: "L'Histoire sans fin",
-  category: "fantasy"
+  category: "Fantasy"
 },{
   author: "Andrzej Sapkowski",
   title: "Le Sorceleur",
-  category: "fantasy"
+  category: "Fantasy"
 },{
   author: "George R. R. Martin",
   title: "Le Trône de fer",
-  category: "fantasy"
+  category: "Fantasy"
 },{
   author: "Hervé Bazin",
   title: "Vipère au poing",
-  category: "autobiographie"
+  category: "Autobiographie"
 },{
   author: "Marie Cardinal",
   title: "Les mots pour le dire",
-  category: "autobiographie"
+  category: "Autobiographie"
 },{
   author: "Giacomo Casanova",
   title: "Histoire de ma vie",
-  category: "autobiographie"
+  category: "Autobiographie"
 }]
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Mini-bibliothèque',
+  res.render('search.ejs', {
+    title: 'Mini-bibliothèque'
   });
 });
 
 router.get('/books/search', function (req, res, next) {
-  res.render('index', {
+  res.render('search.ejs', {
     title: 'Mini-bibliothèque'
   });
 });
 
 
 router.post('/books/list', function (req, res, next){
-  res.render('')
+  const filteredBooks = books.filter(books => books.category === req.body.category)
+  const tabLength = filteredBooks.length
+  res.render('collection.pug',{
+   title : req.body.category,
+   books: filteredBooks,
+   length : tabLength
+
+  })
 });
 
-module.exports = router, books;
+
+module.exports = router;
